6801b1f9d55256b0b69c47f9e3641ac6747abf28
Pentin Alexander Sergeevich <30556064+pentin-as@users.noreply.github.com>
2018-06-20 17:35:21 +0500

    Update init-submodules.sh

--- scripts/init-submodules.sh
+++ scripts/init-submodules.sh
@@ -6,6 +6,6 @@ git submodule update --init
 (cd rocket-chip && git submodule update --init --recursive hardfloat chisel3 firrtl)
 # riscv-tools (skip llvm)
 (cd rocket-chip && git submodule update --init riscv-tools)
-(cd rocket-chip/riscv-tools && git submodule update --init --recursive riscv-isa-sim riscv-fesvr riscv-pk riscv-opcodes riscv-tests riscv-gnu-toolchain riscv-openocd)
+(cd rocket-chip/riscv-tools && git submodule update --init --recursive --depth=200 riscv-isa-sim riscv-fesvr riscv-pk riscv-opcodes riscv-tests riscv-gnu-toolchain riscv-openocd)
 # torture submodules
 (cd torture && git submodule update --init --recursive)
